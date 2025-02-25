a4d44d9cd6c14d1c0b748567a5faca46884fb400
hemanthraj9110 <hemanthraj9110@gmail.com>
2025-02-25 22:16:08 +0530

    this is java file

---
+++ Sample.java
@@ -0,0 +1,9 @@
+package sample;
+
+import java.security.DomainCombiner;
+
+public class Sample {
+public static void main(String[] args) {
+	System.out.println("this is java file for jenkins");
+}
+}
